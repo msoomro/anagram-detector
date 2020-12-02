59fcd4e4e94dbdc78feb059f16543333ea3411f5
Moomal <moomalmsoomro@gmail.com>
2020-12-02 00:25:20 -0500

    commit tree 6e24e07510304c421304bc00b6fad385bef23931

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+
+    # requires an array of possible anagrams
+    # returns all matches in an array. if no matches, returns empty array
+    def match (possible_anagrams)
+        anagrams = []
+
+        if possible_anagrams == nil || possible_anagrams == [] || @word == nil
+            return anagrams
+        end
+
+        possible_anagrams.each do |p_anagram|
+            if @word.split("").sort == p_anagram.split("").sort
+                anagrams << p_anagram
+            end
+        end
+
+        anagrams
+    end
+end
